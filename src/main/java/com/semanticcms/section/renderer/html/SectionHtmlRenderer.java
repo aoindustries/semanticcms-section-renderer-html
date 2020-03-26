3d8e390ff7b4e6dbe8e5853ce2f7593d3e4eb4b8
AO Industries, Inc <support@aoindustries.com>
2020-03-26 07:37:41 -0500

    New methods MediaWriter.text(…) for writing arbitrary textual data.
    Automatically determines correct encoding.  Supports encoder prefixes
    and suffixes as well as in-context translation.

--- src/main/java/com/semanticcms/section/renderer/html/SectionHtmlRenderer.java
+++ src/main/java/com/semanticcms/section/renderer/html/SectionHtmlRenderer.java
@@ -90,7 +90,7 @@ final public class SectionHtmlRenderer {
 				pageIndex,
 				sectioningContent.getPage(),
 				id,
-				new MediaWriter(textInXhtmlAttributeEncoder, html.out)
+				new MediaWriter(html.encodingContext, textInXhtmlAttributeEncoder, html.out)
 			);
 			html.out.write('"');
 		}
